8cbb8a9fddbaef61c3c9a97afa62c6867978d16a
Sarah Townson <sarah.townson@scienceoxford.com>
2018-10-02 14:31:32 +0100

    Update master_die.py
    update e.g. "one" to "1" in dictionary to make sending messages more straightforward

--- master_die.py
+++ master_die.py
@@ -12,9 +12,9 @@ radio.on()
 np = neopixel.NeoPixel(pin0, 7)
 
 # dictionary to associate a string number with the neopixels needed for pattern
-numbers = {"one": [0, 0, 0, 1, 0, 0, 0], "two": [0, 1, 0, 0, 0, 1, 0],
-           "three": [1, 0, 0, 1, 0, 0, 1], "four": [1, 0, 1, 0, 1, 0, 1],
-           "five": [1, 0, 1, 1, 1, 0, 1], "six": [1, 1, 1, 0, 1, 1, 1]}
+numbers = {"1": [0, 0, 0, 1, 0, 0, 0], "2": [0, 1, 0, 0, 0, 1, 0],
+           "3": [1, 0, 0, 1, 0, 0, 1], "4": [1, 0, 1, 0, 1, 0, 1],
+           "5": [1, 0, 1, 1, 1, 0, 1], "6": [1, 1, 1, 0, 1, 1, 1]}
 
 # dictionary to associate a string colour with the RGB values for that colour
 colours = {"red": (255, 0, 0), "green": (0, 255, 0), "blue": (0, 0, 255),
@@ -37,7 +37,7 @@ def show_number(time):
 
 while True:
     message = radio.receive()
-    # message must be in format 'number colour' e.g. 'three red' or 'two green'
+    # message must be in format 'number colour' e.g. '3 red' or '2 green'
     while message is None:
         # if no messages, cycle through random numbers & colours
         number = random.choice(list(numbers.values()))
@@ -45,7 +45,7 @@ while True:
         show_number(200)
         message = radio.receive()
     try:
-        # split message into ["number", "colour"] e.g. ["three", "red"] & show
+        # split message into ["number", "colour"] e.g. ["3", "red"] & show
         roll = message.split()
         number = numbers[roll[0]]
         colour = colours[roll[1]]
